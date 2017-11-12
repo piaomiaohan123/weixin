209969aae15efd523279a388a9de23d0588fe5b9
piaomiaohan <piaomiaohan12@163.com>
2017-11-12 22:06:47 +0800

    banner类测试tp5验证器

--- application/index/controller/Banner.php
+++ application/index/controller/Banner.php
@@ -2,11 +2,23 @@
 
 namespace app\index\controller;
 use app\index\model\Banner as banners;
+use think\Validate;
 
 class Banner
 {
-	public function getBanner($id)
-	{
+        public function getBanner($id)
+        {
+
+                $data=[
+                 'name'=>'piaomiaohan',
+                 'email'=>'123@qq.com'
+                ];
+                $validate=new Validate([
+                   'name'=>'require|max:20',
+                   'email'=>'email'
+                ]);
+                $result=$validate->batch()->check($data);
+                var_dump($validate->getError());
         $data=banners::get($id);
         $code=12345;
         $message='success';
@@ -18,6 +30,6 @@ class Banner
 
 
 
-		return  json_encode($content);
-	}
+                return  json_encode($content);
+        }
 }
